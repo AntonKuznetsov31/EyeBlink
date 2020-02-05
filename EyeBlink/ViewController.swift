5a9ed775a1a1512e7b63a24c27510c4b4501c410
Anton Kuznetsov <anton.kuznetsov.mail@gmail.com>
2020-02-05 18:18:05 +0200

    Initial Commit

---
+++ EyeBlink/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EyeBlink
+//
+//  Created by Anton Kuznetsov on 05.02.2020.
+//  Copyright © 2020 Anton Kuznetsov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
